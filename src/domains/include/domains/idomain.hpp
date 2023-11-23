71ab49f32defaeb09a73d1e5682ba0b5609645ed
Jan Juračka <jan.juracka@tescan.com>
2023-11-23 15:53:30 +0100

    WIP: creating domain interface

---
+++ src/domains/include/domains/idomain.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "program/dict.hpp"
+#include "program/dict_stack.hpp"
+#include "program/instr.hpp"
+#include "program/instr_fast.hpp"
+
+namespace cthu::domains {
+class IDomain {
+  public:
+    call();
+};
+} // namespace cthu::domains
