8f0608c352d734ad823fe01a4b3e20abeb4b0586
Jan Juračka <jan.juracka@tescan.com>
2023-11-27 15:27:38 +0100

    Deleted old code

--- src_old/interpreter/source/interpreter_dummy.cpp
+++
@@ -1,7 +0,0 @@
-#include "interpreter_dummy.h"
-
-namespace cthu::interpreter {
-/* static */ std::unique_ptr<IInterpreter> IInterpreter::createInterpreterDummy() {
-    return std::make_unique<impl::InterpreterDummy>();
-}
-} // namespace cthu::interpreter
--- src_old/parser/source/parser_dummy.cpp
+++
@@ -1,5 +0,0 @@
-#include "parser_dummy.h"
-
-namespace cthu::parser {
-/* static */ std::unique_ptr<IParser> IParser::createParserDummy() { return std::make_unique<impl::ParserDummy>(); }
-} // namespace cthu::parser
